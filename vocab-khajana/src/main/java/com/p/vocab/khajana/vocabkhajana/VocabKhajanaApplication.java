832a849b6921b097904ab055b3eb518b1cff8ebe
Premendra.Kumar <premendra.bce05515@gmail.com>
2020-01-26 22:35:07 +0530

    Initial commit

---
+++ vocab-khajana/src/main/java/com/p/vocab/khajana/vocabkhajana/VocabKhajanaApplication.java
@@ -0,0 +1,13 @@
+package com.p.vocab.khajana.vocabkhajana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabKhajanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabKhajanaApplication.class, args);
+	}
+
+}
